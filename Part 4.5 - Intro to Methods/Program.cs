a72511a72efd9c266eef256fca76400fd8381bd9
Jack Gallagher <339690067@GoTVDSB.ca>
2024-09-12 08:58:05 -0400

    Finished Tutorial

--- Part 4.5 - Intro to Methods/Program.cs
+++ Part 4.5 - Intro to Methods/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Press ENTER for a joke:");
+            Console.ReadLine();
+            Joke(99);
+        }
+
+        public static void Joke(int numBugs)
+        {
+            Console.WriteLine(numBugs + " little bugs in the code");
+            Thread.Sleep(500);
+            Console.WriteLine(numBugs + " little bugs");
+            Thread.Sleep(500);
+            Console.WriteLine("Fix a bug, run it again,");
+            Thread.Sleep(500);
+            Console.WriteLine((numBugs + 1) + " little bugs in the code");
         }
     }
 }
